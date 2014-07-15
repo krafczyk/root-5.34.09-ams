c3d64e7f049b0ce90dc2f82b3b023848d1d3795e
pzuccon <pzuccon>
2014-07-15 20:36:56 +0000

    another fix

---
+++ core/utils/src/VariableSelectionRule.cxx
@@ -0,0 +1,18 @@
+// @(#)root/core/utils:$Id$
+// Author: Velislava Spasova September 2010
+
+/*************************************************************************
+ * Copyright (C) 1995-2011, Rene Brun, Fons Rademakers and al.           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// VariableSelectionRule                                                       //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#include "VariableSelectionRule.h"
